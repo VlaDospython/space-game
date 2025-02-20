c146a77073ce84a2d1ff8aa6eacde1a5ab56c918
Vitaliy Svyatodukh <v.sviatodukh@m2e.team>
2025-02-20 18:30:44 +0200

    strategy beginning

--- src/main.py
+++ src/main.py
@@ -5,6 +5,32 @@ import os
 from src.constants import *
 from src.player import Player
 from src.meteor import Meteor
+from abc import ABC, abstractmethod
+
+
+class Context:
+    def set_strategy(self, strat):
+        self.strategy = strat
+
+    def get_surface(self):
+        return self.strategy.get_surface()
+
+
+class Strategy(ABC):
+    @abstractmethod
+    def get_surface(self):
+        pass
+
+
+class SimpleImage(Strategy):
+    def __init__(self):
+        self.image = pygame.Surface((25, 40))
+        self.image.fill(RED)
+
+    def get_surface(self):
+        return self.image
+
+
 
 # Load images
 img = pygame.image.load(BG_IMG)
@@ -36,6 +62,10 @@ for _ in range(random.randint(15, 30)):
 while running:
     clock.tick(FPS)
     print(f"Використано пам'яті: {process.memory_info().rss / 1024 / 1024:.2f}/{total_memory_mb:.2f} MB")
+
+    if process.memory_info().rss / 1024 / 1024 > 100:
+        running = False
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
